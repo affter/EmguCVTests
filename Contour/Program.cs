c8570328a5b869d1ce64b8abb5b676eb51dd27d8
affter <affter@users.noreply.github.com>
2018-08-21 21:21:37 +0400

    contour selection finished

--- Contour/Program.cs
+++ Contour/Program.cs
@@ -11,24 +11,30 @@ namespace Contour
     {
         static void Main(string[] args)
         {
-            var img = CvInvoke.Imread("../../../source/human.png", ImreadModes.Grayscale);
-            UMat edges = new UMat();
-            UMat edges2 = new UMat();
+            var img = new Image<Gray, byte>("../../../source/human.png");
+            var top = img.ThresholdBinary(new Gray(174), new Gray(255));
+            var bottom = img.ThresholdBinary(new Gray(175), new Gray(255));
+            var binaryImg = top.Sub(bottom);
+            CvInvoke.Erode(binaryImg, binaryImg, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
+            CvInvoke.Dilate(binaryImg, binaryImg, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-            UMat newEdges = new UMat();
-            CvInvoke.Canny(img, edges, 20, 100, 3);
-            CvInvoke.Imshow("edges", edges);
-            CvInvoke.Dilate(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
-            CvInvoke.Erode(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
-            CvInvoke.FindContours(edges, contours, newEdges, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
-            CvInvoke.Canny(img, newEdges, 20, 180, 3);
-            CvInvoke.AbsDiff(edges, newEdges, edges2);
-            CvInvoke.AbsDiff(edges, edges2, edges2);
+            CvInvoke.FindContourTree(binaryImg, contours, ChainApproxMethod.ChainApproxSimple);
             CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
-            CvInvoke.Imshow("newEdges", edges2);
-            CvInvoke.Imshow("edges", edges);
-            CvInvoke.Imshow("grayscale", img);
+            CvInvoke.Imshow("White man", img);
             CvInvoke.WaitKey();
+            //UMat edges = new UMat();
+            //VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            //UMat newEdges = new UMat();
+            //CvInvoke.Canny(img, edges, 20, 100, 3);
+            //CvInvoke.Imshow("edges", edges);
+            //CvInvoke.Dilate(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
+            //CvInvoke.Erode(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
+            //CvInvoke.FindContours(edges, contours, newEdges, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
+            //CvInvoke.Canny(img, newEdges, 20, 180, 3);
+            //CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
+            //CvInvoke.Imshow("edges", edges);
+            //CvInvoke.Imshow("grayscale", img);
+            //CvInvoke.WaitKey();
         }
     }
 }
