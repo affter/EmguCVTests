7d7c14b23af902f4c9930005c91e28385f13bd29
affter <affter@users.noreply.github.com>
2018-08-21 20:18:39 +0400

    contour in progress

---
+++ Contour/Program.cs
@@ -0,0 +1,34 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Drawing;
+
+namespace Contour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img = CvInvoke.Imread("../../../source/human.png", ImreadModes.Grayscale);
+            UMat edges = new UMat();
+            UMat edges2 = new UMat();
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            UMat newEdges = new UMat();
+            CvInvoke.Canny(img, edges, 20, 100, 3);
+            CvInvoke.Imshow("edges", edges);
+            CvInvoke.Dilate(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
+            CvInvoke.Erode(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
+            CvInvoke.FindContours(edges, contours, newEdges, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.Canny(img, newEdges, 20, 180, 3);
+            CvInvoke.AbsDiff(edges, newEdges, edges2);
+            CvInvoke.AbsDiff(edges, edges2, edges2);
+            CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
+            CvInvoke.Imshow("newEdges", edges2);
+            CvInvoke.Imshow("edges", edges);
+            CvInvoke.Imshow("grayscale", img);
+            CvInvoke.WaitKey();
+        }
+    }
+}
---
+++ Contour/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Contour")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Contour")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("fc4b2b94-b877-4377-946a-aeaea2d5c9ae")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
