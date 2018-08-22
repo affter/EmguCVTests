2a3fde79c8162f8ed929a7e3627eb6ab0d2328c6
affter <affter@users.noreply.github.com>
2018-08-22 08:27:41 +0400

    normals finished, started normals without edges

--- Contour/Program.cs
+++ Contour/Program.cs
@@ -3,7 +3,10 @@ using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using System;
+using System.Linq;
 using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Contour
 {
@@ -11,30 +14,99 @@ namespace Contour
     {
         static void Main(string[] args)
         {
-            var img = new Image<Gray, byte>("../../../source/human.png");
-            var top = img.ThresholdBinary(new Gray(174), new Gray(255));
-            var bottom = img.ThresholdBinary(new Gray(175), new Gray(255));
+            var img = new Image<Bgr, byte>("../../../source/human.png");
+            Image<Gray, Byte> grayImage = img.Convert<Gray, Byte>();
+            int y = 0;
+            var top = grayImage.ThresholdBinary(new Gray(174), new Gray(255));
+            var bottom = grayImage.ThresholdBinary(new Gray(175), new Gray(255));
             var binaryImg = top.Sub(bottom);
             CvInvoke.Erode(binaryImg, binaryImg, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
             CvInvoke.Dilate(binaryImg, binaryImg, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
             CvInvoke.FindContourTree(binaryImg, contours, ChainApproxMethod.ChainApproxSimple);
-            CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
-            CvInvoke.Imshow("White man", img);
+            CvInvoke.DrawContours(img, contours, -1, new MCvScalar(0, 0, 255));
+            var imgForNormalsWithoutSharpEdges = img.Clone();
+            CvInvoke.Imshow("Contour", img);
+            Point startPoint = Point.Empty;
+            Point endPoint = Point.Empty;
+            for (int i = 0; i < contours[0].Size-10; i+=10)
+            {
+                CalculateNormal(img, contours[0][i], contours[0][i + 10], out startPoint, out endPoint);
+                CvInvoke.Line(img, contours[0][i], contours[0][i + 10], new MCvScalar(0, 255, 0));
+                CvInvoke.Line(img, startPoint, endPoint, new MCvScalar(0, 255, 0));
+                if (i+20 > contours[0].Size)
+                {
+                    CalculateNormal(img, contours[0][i+10], contours[0][0], out startPoint, out endPoint);
+                    CvInvoke.Line(img, contours[0][i+10], contours[0][0], new MCvScalar(0, 255, 0));
+                    CvInvoke.Line(img, startPoint, endPoint, new MCvScalar(0, 255, 0));
+                }
+            }
+            CvInvoke.Imshow("Normals", img);
+            var normalBases = CalculateNormalBases(contours);
+            foreach (var normalBase in normalBases)
+            {
+                CalculateNormal(imgForNormalsWithoutSharpEdges, normalBase.Item1, normalBase.Item2, out startPoint, out endPoint);
+                CvInvoke.Line(imgForNormalsWithoutSharpEdges, normalBase.Item1, normalBase.Item2, new MCvScalar(0, 255, 0));
+                CvInvoke.Line(imgForNormalsWithoutSharpEdges, startPoint, endPoint, new MCvScalar(0, 255, 0));
+            }
+            CvInvoke.Imshow("ImageWithNormalsWithoutSharpEdges", imgForNormalsWithoutSharpEdges);
             CvInvoke.WaitKey();
-            //UMat edges = new UMat();
-            //VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-            //UMat newEdges = new UMat();
-            //CvInvoke.Canny(img, edges, 20, 100, 3);
-            //CvInvoke.Imshow("edges", edges);
-            //CvInvoke.Dilate(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
-            //CvInvoke.Erode(edges, edges, new Mat(), new Point(-1, -1), 1, BorderType.Default, new MCvScalar(255, 0, 0));
-            //CvInvoke.FindContours(edges, contours, newEdges, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
-            //CvInvoke.Canny(img, newEdges, 20, 180, 3);
-            //CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
-            //CvInvoke.Imshow("edges", edges);
-            //CvInvoke.Imshow("grayscale", img);
-            //CvInvoke.WaitKey();
+        }
+
+        private static IEnumerable<Tuple<Point,Point>> CalculateNormalBases(VectorOfVectorOfPoint contours, double alpha = 5, double beta = 6)
+        {
+            var t = new List<Tuple<Point, Point>>();
+            for (int i = 0; i < contours[0].Size - 1; i++)
+            {
+                Point start = contours[0][i];
+                Point end = contours[0][++i];
+                double length = GetLength(start, end);
+                while (length < alpha && i != contours[0].Size - 1)
+                {
+                    end = contours[0][++i];
+                    length = GetLength(start, end);
+                }
+                length = GetLength(start, end);
+                if (length > beta)
+                {
+                   t.Add(new Tuple<Point, Point>(start, end));
+                }
+            }
+            return t;
+        }
+
+        private static double GetLength(Point start, Point end)
+        {
+            return Math.Sqrt((end.X - start.X) * (end.X - start.X) + (end.Y - start.Y) * (end.Y - start.Y));
+        }
+
+        private static void CalculateNormal(CvArray<byte> img, Point point1, Point point2, out Point startPoint, out Point endPoint, int normalLength = 10)
+        {
+            int startX = (point1.X + point2.X) / 2;
+            int startY = (point1.Y + point2.Y) / 2;
+            double coeff = (double)(point2.Y - point1.Y) / (point2.X - point1.X);
+
+            Console.WriteLine(coeff);
+            int endX;
+            if (point2.Y < point1.Y)
+            {
+                endX = (int)Math.Round(normalLength / Math.Sqrt(1 + 1 / (coeff * coeff)) + startX);
+            }
+            else
+            {
+                endX = (int)Math.Round(-normalLength / Math.Sqrt(1 + 1 / (coeff * coeff)) + startX);
+            }
+            int endY;
+            if (coeff == 0)
+            {
+                endY = startY - normalLength;
+            }
+            else
+            {
+                endY = (int)Math.Round(startY + startX / coeff - endX / coeff);
+            }
+            startPoint = new Point(startX, startY);
+            endPoint = new Point(endX, endY);
         }
     }
 }
